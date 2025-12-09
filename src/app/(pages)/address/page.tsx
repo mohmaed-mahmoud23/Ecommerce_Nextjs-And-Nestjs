67edeb4f766bead620c77aa4d4f7f37e01bdcf4f
mohmaed-mahmoud23 <mohamedmahmoud43467@gmail.com>
2025-12-09 16:29:50 +0000

    ubdted error Next.js

--- src/app/(pages)/address/page.tsx
+++ src/app/(pages)/address/page.tsx
@@ -3,7 +3,7 @@ import AddressFormClient from "./AddressFormClient";
 
 export default function Page() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-6 bg-gray-100">
+    <div>
       <Suspense fallback={<p>Loading...</p>}>
         <AddressFormClient />
       </Suspense>
