32725f14f26e51334bed59e0a37c225853388b22
mohmaed-mahmoud23 <mohamedmahmoud43467@gmail.com>
2025-12-16 20:32:29 +0000

    Serch

--- src/app/(pages)/address/AddressFormClient.tsx
+++ src/app/(pages)/address/AddressFormClient.tsx
@@ -82,7 +82,7 @@ const Page = () => {
         }
 
         const response = await fetch(
-          `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=http://localhost:3000/`,
+          `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=http://localhost:3000`,
           {
             method: "POST",
             headers: {
